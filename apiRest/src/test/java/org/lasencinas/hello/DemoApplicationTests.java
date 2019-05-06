5e11bc13516f456b95c924ca9a2c8fa22732f083
marc monjon <sysmarc13@hotmail.com>
2019-05-06 23:19:51 +0200

    Segundo caso test pasado

--- apiRest/src/test/java/org/lasencinas/hello/DemoApplicationTests.java
+++ apiRest/src/test/java/org/lasencinas/hello/DemoApplicationTests.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,11 +24,22 @@ public class DemoApplicationTests {
 	private MockMvc mockMvc;
 
 	@Test
-	public void comprobarMetodoSayHi() throws Exception {
+	public void getSayHiMethod() throws Exception {
 
 		this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isOk())
 				.andExpect(content().string(equalTo("Hi")));
 	}
+	
+	@Test
+    public void getAllTopicsTest() throws Exception {
+        mockMvc.perform(get("/topics").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json("[{id : 'spring', name : 'Spring Framework', description : " +
+                        "'Spring Framework Description'}, " +
+                        "{id : 'java', name : 'Core Java', description : 'Core Java Description'}, " +
+                        "{id : 'javascript', 'name' : 'JavaScript', 'description' : 'JavaScript Description'}]"));
+    }
 	
 	
 
